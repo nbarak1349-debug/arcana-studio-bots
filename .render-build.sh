eadff581aa6f179a0415ee1f52eadfa6c2dffa40
nikita <n.barak1349@gmail.com>
2026-02-28 14:13:56 +0300

    Add render build script

---
+++ .render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Удаляем старое виртуальное окружение, если оно есть
+rm -rf .venv
+# Устанавливаем зависимости заново
+pip install -r requirements.txt#!/usr/bin/env bash
+# Удаляем старое виртуальное окружение, если оно есть
+rm -rf .venv
+# Устанавливаем зависимости заново
+pip install -r requirements.txt
